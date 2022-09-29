b8eb799e5c678b72f4ae31232f30f0b74962448d
Mahmoud elsawah <hxxcvhh122@gmail.com>
2022-09-30 00:25:55 +0200

    “new-update”

--- src/component/earth/index.js
+++ src/component/earth/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import * as THREE from 'three'
 import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
 import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
@@ -7,34 +7,57 @@ import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
 import { useFrame, useLoader } from '@react-three/fiber';
 import { TextureLoader } from 'three';
 import { OrbitControls, Stars } from '@react-three/drei';
- const Earth = () => {
-    const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(TextureLoader,[EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]);
-    const earthRef = useRef();
-    const cloudRef = useRef();
+const Earth = () => {
+  const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(TextureLoader, [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]);
+  const earthRef = useRef();
+  const cloudRef = useRef();
+  const [size, setSize] = useState(3);
+  // let width = screen.width;
+  
+  useEffect(() => {
+    if (window.screen.width < 461) {
+      setSize(1)
 
-    useFrame(({clock})=>{
-        const elapsedTime = clock.getElapsedTime();
-      earthRef.current.rotation.y = elapsedTime /6
-      cloudRef.current.rotation.y = elapsedTime /6
+    } else if (window.screen.width > 461 && window.screen.width < 768) {
+      setSize(2)
+    }
+    window.addEventListener('resize', () => {
+      if (window.screen.width < 461) {
+        setSize(1)
 
-    },)
+      } else if (window.screen.width > 461 && window.screen.width < 768) {
+        setSize(2)
+      }
+    })
+  })
+  // setSize(2);
+
+  console.log(window.screen.width);
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    earthRef.current.rotation.y = elapsedTime / 6
+    cloudRef.current.rotation.y = elapsedTime / 6
+
+  },)
+  // setSize(size-1)
+
   return (
     <>
-        <ambientLight intensity={0.1}/>
-        <pointLight color={'#f6f3ea'} position={[2 , 0 , 5]} intensity={1.2}/>
-        {/* <Stars radius={300} depth={60} count={20000} factor={8} saturation={0} fade={true}/> */}
-        <mesh ref={cloudRef}  position={[0, 0 , 3]}>
-            <sphereGeometry args={[1.005, 32, 32]} />
-            <meshPhongMaterial map={cloudsMap} opacity={0.4} depthWrite={true} transparent={true} side={THREE.DoubleSide}/>
+      <ambientLight intensity={0.1} />
+      <pointLight color={'#ffffff'} position={[2, 0, 5]} intensity={1.2} />
+      <Stars radius={600} depth={60} count={20000} factor={8} saturation={0} fade={true} />
+      <mesh ref={cloudRef} position={[0, 0, size]}>
+        <sphereGeometry args={[1.005, 32, 32]} />
+        <meshPhongMaterial map={cloudsMap} opacity={0.4} depthWrite={true} transparent={true} side={THREE.DoubleSide} />
 
-        </mesh>
+      </mesh>
 
-        <mesh ref={earthRef} position={[ 0, 0, 3]}>
-        <sphereGeometry args={[1, 32, 32]}/>
-        <meshPhongMaterial specularMap={specularMap}  />
-        <meshStandardMaterial map={colorMap} normalMap={normalMap} metalness={0.4} roughness={0.7}/>
-        <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} zoomSpeed={0.6} panSpeed={0.5} rotateSpeed={0.4}/>
-        </mesh>
+      <mesh ref={earthRef} position={[0, 0, size]}>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshPhongMaterial specularMap={specularMap} />
+        <meshStandardMaterial map={colorMap} normalMap={normalMap} metalness={0.4} roughness={0.7} />
+        <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} zoomSpeed={0} panSpeed={0} rotateSpeed={0} />
+      </mesh>
     </>
   )
 }
--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -1,4 +1,4 @@
-import  React, { useState } from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -11,67 +11,78 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import { Link } from 'react-router-dom';
- const Navbar =()=> {
-    const [open , setOpen] = useState(false);
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import CallIcon from '@mui/icons-material/Call';
+const menuItems = [
+  {
+    icon: (<HomeIcon />),
+    text: "Home",
+  },
+  {
+    icon: (<InfoIcon />),
+    text: "About",
+  },
+
+  {
+    icon: (<CallIcon />),
+    text: "Contact",
+  }
+]
+const Navbar = () => {
+
+  const [open, setOpen] = useState(false);
   return (
-    <Box sx={{ flexGrow: 1  , width : '100%'  }}>
-      <AppBar position="static" sx={{ position : 'absolute' , zIndex:'100' , backgroundColor : 'transparent' , boxShadow: 'none'}}>
-        <Toolbar sx={{marginTop : '10px'}}>
-          <Link to={'/'}  style={{ flexGrow: 1}}>
-          <Typography variant="h4" fontSize={30} fontWeight={'bold'} component="div" sx={{ flexGrow: 1}}>
-             nasa space
-          </Typography>
-          </Link>
-          <Box sx={{display : {sm : 'flex' , xs : 'none'} , width : {lg : '24%' , md : '35%' , sm : '50%'} ,  justifyContent: 'space-evenly' , alignItems: 'center' }}>
-          <Typography fontSize={18} fontWeight={500}>Home</Typography>
-          <Typography fontSize={18} fontWeight={500}>About</Typography> 
-          <Typography fontSize={18} fontWeight={500}>Details</Typography>
-          <Typography fontSize={18} fontWeight={500}>contact</Typography>
+    <Box>
+      <AppBar sx={{backgroundColor : 'transparent', boxShadow : 'none'}}>
+        <Toolbar>
+            <Typography sx={{ flexGrow: 1 , width : '55%' }} variant="h4" fontSize={30} fontWeight={'bold'} component="div" sx={{ flexGrow: 1 }}>
+              <Link to={'/'}>
+              nasa space
+              </Link>
+            </Typography>
+          <Box sx={{ display: { sm: 'flex', xs: 'none' }, width: { xl : '20%' , lg : '25%' , sm : '45%'} ,justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography fontSize={18} fontWeight={500}><Link to={'/home'}>Home</Link></Typography>
+            <Typography fontSize={18} fontWeight={500}><Link to={'/issTracker'}>Iss Tracker</Link></Typography>
+            <Typography fontSize={18} fontWeight={500}>Details</Typography>
+            <Typography fontSize={18} fontWeight={500}>contact</Typography>
           </Box>
-          <Box sx={{display : {sm : 'none' , xs : 'flex'}}}>
-          <IconButton onClick={()=> setOpen(true)}
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Drawer anchor='right' open={open} onClose={()=>setOpen(false)}>
-             <Box sx={{width : '250px'}}>
-             <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>             </Box>
-          </Drawer>
+          <Box sx={{ display: { sm: 'none', xs: 'flex' } }}>
+            <IconButton onClick={() => setOpen(true)}
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Drawer anchor='right' open={open} onClose={() => setOpen(false)} >
+              <Box sx={{ width: '250px', bgcolor: '#000', height: '100%', color: 'white' }}>
+                <List >
+             
+                  {menuItems.map((item, index) => (
+                    <ListItem key={index}>
+                      <ListItemButton>
+                      
+                      <ListItemIcon sx={{ color: "white" }}>
+                        {item.icon}
+                      </ListItemIcon>
+                      <ListItemText primary={item.text} />
+
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </List>
+                <Divider />
+              </Box>
+            </Drawer>
           </Box>
         </Toolbar>
       </AppBar>
     </Box>
   );
 }
+
+
 export default Navbar
